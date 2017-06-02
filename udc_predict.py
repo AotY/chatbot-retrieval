e04ac9c8683c98f2225889c1e513e2ffd4fc9958
LeonTao <QingTaoGg@gmail.com>
2017-06-03 00:05:52 +0800

    fix udc_predict

--- udc_predict.py
+++ udc_predict.py
@@ -1,9 +1,14 @@
+import codecs
 import os
 import time
 import itertools
 import sys
+
+import jieba
 import numpy as np
 import tensorflow as tf
+from pybloom import BloomFilter
+
 import udc_model
 import udc_hparams
 import udc_metrics
@@ -13,11 +18,63 @@ from models.helpers import load_vocab
 
 tf.flags.DEFINE_string("model_dir", None, "Directory to load model checkpoints from")
 tf.flags.DEFINE_string("vocab_processor_file", "./data/BoP2017_DBAQ_dev_train_data/vocab_processor.bin", "Saved vocabulary processor file")
+
+tf.flags.DEFINE_string(
+    "input_dir", os.path.abspath("./data/BoP2017_DBAQ_dev_train_data/"),
+    "Input directory containing original CSV data files (default = './data/BoP2017_DBAQ_dev_train_data/')")
+
+tf.flags.DEFINE_string(
+    "output_dir", os.path.abspath("./data/BoP2017_DBAQ_dev_train_data/"),
+    "Output directory for TFrEcord files (default = './..data/BoP2017_DBAQ_dev_train_data/')")
+
+
 FLAGS = tf.flags.FLAGS
 
 if not FLAGS.model_dir:
   print("You must specify a model directory")
   sys.exit(1)
+
+
+
+# 结巴分词进行初始化
+def init_jieba():
+    # 加载用户词典
+    jieba.load_userdict(os.path.join(FLAGS.input_dir, "userdict.txt"))
+    pass
+
+
+init_jieba()
+
+bloomFilter = BloomFilter(capacity=1000, error_rate=0.001)
+
+
+# 加载停顿词
+def load_stop_word():
+    with codecs.open(os.path.join(FLAGS.input_dir, "stopword.txt"), 'rb', encoding='utf-8') as f:
+        for line in f:
+            bloomFilter.add(line.rstrip())
+
+
+load_stop_word()
+
+
+# 分词
+def tokenizer_fn(iterator):
+    # return (x.split(" ") for x in iterator)
+    # # 精确模式 HMM 参数用来控制是否使用 HMM 模型  于未登录词，采用了基于汉字成词能力的 HMM 模型，使用了 Viterbi 算法
+    for x in iterator:
+        # seg_list = jieba.cut(x, cut_all=False, HMM=True)
+        seg_list = jieba.cut(x, cut_all=True, HMM=True) #精确模式
+        # seg_list = jieba.cut_for_search("小明硕士毕业于中国科学院计算所，后在日本京都大学深造")  # 搜索引擎模式
+        # print('seg_list', seg_list)
+        no_stop_list = remove_stop(seg_list)
+        yield no_stop_list
+
+
+# 去除停顿词
+def remove_stop(seg_list):
+    return [word for word in seg_list if word not in bloomFilter]
+
 
 
 # Load vocabulary
@@ -45,6 +102,9 @@ def get_features(question, anwser):
 if __name__ == "__main__":
   hparams = udc_hparams.create_hparams()
   model_fn = udc_model.create_model_fn(hparams, model_impl=dual_encoder_model)
+
+
+
   estimator = tf.contrib.learn.Estimator(model_fn=model_fn, model_dir=FLAGS.model_dir)
 
   # Ugly hack, seems to be a bug in Tensorflow
@@ -52,6 +112,7 @@ if __name__ == "__main__":
   estimator._targets_info = tf.contrib.learn.estimators.tensor_signature.TensorSignature(
     tf.constant(0, shape=[1,1])
   )
+
 
   print("question: {}".format(INPUT_question))
   for r in POTENTIAL_RESPONSES:
